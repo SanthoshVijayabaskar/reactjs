ddf67c6036059de88e4f13077872c27e6562d08d
Santhosh Vijay <santhoshthepro@gmail.com>
2017-01-15 14:01:51 +0530

    React Router Lesson

---
+++ code/L6/React Page Routing using React-Router/components/contact/contact-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class ContactPage extends React.Component{
+
+    render(){
+        return (
+            <div>
+                    <h1>Call us @ 1800 000 000</h1>
+            </div>
+        )
+    }
+}
+
+export default ContactPage;
