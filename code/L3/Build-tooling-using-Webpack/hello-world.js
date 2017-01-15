51c4a95eca6f1c1edfbae1aaa916e4e78f9e773f
Santhosh Vijay <santhoshthepro@gmail.com>
2017-01-15 09:33:37 +0530

    Build Tooling using Webpack

---
+++ code/L3/Build-tooling-using-Webpack/hello-world.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class HelloWorld extends React.Component{
+
+	render(){
+		return <h1>Welcome to Webpack!</h1>
+	}
+}
+
+export default HelloWorld
---
+++ code/L3/Build-tooling-using-Webpack/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	entry: './app.js',
+	output: {
+		path: './',
+		filename:'bundle.js'
+	},
+
+	devServer: {
+		inline: true,
+		port: 8080
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				// query: {
+				// 	presets: ['es2015','react']
+				// } - Defined in babelrc
+			}
+		]
+	}
+}
